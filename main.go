00673ebfa74a375a36a40981570e64a6ed3919e2
flamingo <490493499@qq.com>
2022-11-24 14:43:52 +0800

    读取请求数据

--- main.go
+++ main.go
@@ -35,7 +35,16 @@ func articlesIndexHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func articlesStoreHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "创建新的文章")
+	if err := r.ParseForm(); err != nil {
+		// 解析错误，这里应该有错误处理
+		fmt.Fprint(w, "请提供正确的数据！")
+		return
+	}
+
+	fmt.Fprintf(w, "r.Form 中 title 的值为: %v <br>", r.FormValue("title"))
+	fmt.Fprintf(w, "r.PostForm 中 title 的值为: %v <br>", r.PostFormValue("title"))
+	fmt.Fprintf(w, "r.Form 中 test 的值为: %v <br>", r.FormValue("test"))
+	fmt.Fprintf(w, "r.PostForm 中 test 的值为: %v <br>", r.PostFormValue("test"))
 }
 
 func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
@@ -43,14 +52,14 @@ func articlesCreateHandler(w http.ResponseWriter, r *http.Request) {
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>创建文章 —— 我的技术博客</title>
+	<title>创建文章 —— 我的技术博客</title>
 </head>
 <body>
-    <form action="%s" method="post">
-        <p><input type="text" name="title"></p>
-        <p><textarea name="body" cols="30" rows="10"></textarea></p>
-        <p><button type="submit">提交</button></p>
-    </form>
+	<form action="%s?test=data" method="post">
+		<p><input type="text" name="title"></p>
+		<p><textarea name="body" cols="30" rows="10"></textarea></p>
+		<p><button type="submit">提交</button></p>
+	</form>
 </body>
 </html>
 `
