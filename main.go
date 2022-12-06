8aeb0fa071f7446d655cc79cd0a13e54df66f868
flamingo <490493499@qq.com>
2022-12-06 17:52:04 +0800

    集成 GORM

--- main.go
+++ main.go
@@ -383,6 +383,7 @@ func main() {
 	database.Initialize()
 	db = database.DB
 
+	bootstrap.SetupDB()
 	router = bootstrap.SetupRoute()
 
 	router.HandleFunc("/articles", articlesIndexHandler).Methods("GET").Name("articles.index")
